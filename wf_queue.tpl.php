67cf791b2f29822eb698ac3cd47cfd0bba2295da
Tuomas Leppänen <tuomas.leppanen@wunderkraut.com>
2013-02-06 22:28:03 +0200

    Built manage page

--- wf_queue.tpl.php
+++ wf_queue.tpl.php
@@ -1,9 +1,3 @@
-<?php
-echo "<pre>";
-print_r($variables);
-echo "</pre>";
-?>
-
 <table>
 <thead><tr>
 <?php
@@ -21,9 +15,32 @@ foreach($variables['submissions'] as $submission) {
   }
   echo "</tr>";
 }
+dsm($variables);
 ?>
 </tbody>
 
 
-
 </table>
+<?php
+echo $variables['queue'] . '<br/><br/>' . t('<h2>Cancellations</h2>');
+$submissions = array();
+foreach ($variables['cancel'] as $cancelUser) {
+  $uid = $cancelUser['uid'];
+  $user = user_load($uid);
+  echo '<b>' . t('User ') . l($user->name, 'user/' . $uid) . ',</b><br/>' . ' has canceled and has the following submissions:<br/>';
+  $submissions = $cancelUser['submissions'];
+  foreach ($cancelUser['submissions'] as $sub => $submissionData) {
+      $sid = $submissionData['sid'];
+      $nid = $submissionData['nid'];
+      $action = $submissionData['action'];
+      echo t(ucfirst($action)) . 
+            ': ' . 
+            l(t('Delete'), 'node/' . $nid . '/submission/' . $sid . '/delete?destination=node/' . $nid . '/manage-event') . 
+            ' | ' . 
+            l(t('View'), 'node/' . $nid . '/submission/' . $sid) . '<br/>';
+    
+  }
+  echo '<br/>';
+}
+
+?>
