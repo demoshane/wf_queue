50f8fa3a8a8cb2f4051e3ca346cfb692c42f91bb
Tuomas Leppänen <tuomas.leppanen@wunderkraut.com>
2013-02-07 08:28:58 +0200

    Added classes to event management page

--- wf_queue.tpl.php
+++ wf_queue.tpl.php
@@ -1,4 +1,4 @@
-<table>
+<table class="event-management-table">
 <thead><tr>
 <?php
 foreach($variables['headers'] as $header) {
@@ -18,29 +18,29 @@ foreach($variables['submissions'] as $submission) {
 dsm($variables);
 ?>
 </tbody>
-
-
 </table>
 <?php
-echo $variables['queue'] . '<br/><br/>' . t('<h2>Cancellations</h2>');
+echo $variables['queue'] . '<br/><br/>' . t('<h2 class="cancellations-header">Cancellations</h2>');
 $submissions = array();
 foreach ($variables['cancel'] as $cancelUser) {
   $uid = $cancelUser['uid'];
   $user = user_load($uid);
-  echo '<b>' . t('User ') . l($user->name, 'user/' . $uid) . ',</b><br/>' . ' has canceled and has the following submissions:<br/>';
+  echo '<div class="cancel-user-subheader"><b>' . t('User ') . l($user->name, 'user/' . $uid) . ',</b></div><div class="cancel-submissions"><br/>' . ' has canceled and has the following submissions:<br/>';
   $submissions = $cancelUser['submissions'];
   foreach ($cancelUser['submissions'] as $sub => $submissionData) {
       $sid = $submissionData['sid'];
       $nid = $submissionData['nid'];
       $action = $submissionData['action'];
-      echo t(ucfirst($action)) . 
+      echo '<div class="submission-result">' . 
+            t(ucfirst($action)) . 
             ': ' . 
             l(t('Delete'), 'node/' . $nid . '/submission/' . $sid . '/delete?destination=node/' . $nid . '/manage-event') . 
             ' | ' . 
-            l(t('View'), 'node/' . $nid . '/submission/' . $sid) . '<br/>';
+            l(t('View'), 'node/' . $nid . '/submission/' . $sid) . '</div>'
+            ;
     
   }
-  echo '<br/>';
+  echo '</div><br/>';
 }
 
 ?>
