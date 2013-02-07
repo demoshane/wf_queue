49b504147489caeda910f0e104ab46268229a516
Tuomas Leppänen <tuomas.leppanen@wunderkraut.com>
2013-02-07 10:04:57 +0200

    Added css styles

--- wf_queue.tpl.php
+++ wf_queue.tpl.php
@@ -1,6 +1,7 @@
 <table class="event-management-table">
 <thead><tr>
 <?php
+// Build headers
 foreach($variables['headers'] as $header) {
   echo "<th>" . $header . "</th>";
 }
@@ -8,6 +9,7 @@ foreach($variables['headers'] as $header) {
 </tr></thead>
 <tbody>
 <?php
+// Loop submission result data
 foreach($variables['submissions'] as $submission) {
   echo "<tr>";
   foreach($submission['fields'] as $field) {
@@ -15,32 +17,44 @@ foreach($variables['submissions'] as $submission) {
   }
   echo "</tr>";
 }
-dsm($variables);
 ?>
 </tbody>
 </table>
 <?php
-echo $variables['queue'] . '<br/><br/>' . t('<h2 class="cancellations-header">Cancellations</h2>');
+// Build cancellation queue link and header for cancellations
+echo '<div class="queue-link">' . 
+     $variables['queue'] . 
+     '</div>' . 
+     t('<h2 class="cancellations-header">Cancellations</h2>')
+     ;
 $submissions = array();
+
+// Loop users that have cancellations
 foreach ($variables['cancel'] as $cancelUser) {
   $uid = $cancelUser['uid'];
   $user = user_load($uid);
-  echo '<div class="cancel-user-subheader"><b>' . t('User ') . l($user->name, 'user/' . $uid) . ',</b></div><div class="cancel-submissions"><br/>' . ' has canceled and has the following submissions:<br/>';
+  // Build markup
+  echo '<div class="submissions-container"><div class="cancel-user-subheader"><b>' . 
+       t('User ') . 
+       l($user->name, 'user/' . $uid) . ',</b></div><div class="cancel-submissions">' . 
+       ' has canceled and has the following submissions:'
+       ;
   $submissions = $cancelUser['submissions'];
+
+  // Loop all submissions for user that has canceled their participation. For easy editing and grouping for admins.
   foreach ($cancelUser['submissions'] as $sub => $submissionData) {
       $sid = $submissionData['sid'];
       $nid = $submissionData['nid'];
       $action = $submissionData['action'];
-      echo '<div class="submission-result">' . 
+      // Build markup
+      echo '<div class="submission-result"><div class="submission-action">' . 
             t(ucfirst($action)) . 
-            ': ' . 
+            ':</div><div class="submission-tools"> ' . 
             l(t('Delete'), 'node/' . $nid . '/submission/' . $sid . '/delete?destination=node/' . $nid . '/manage-event') . 
             ' | ' . 
-            l(t('View'), 'node/' . $nid . '/submission/' . $sid) . '</div>'
+            l(t('View'), 'node/' . $nid . '/submission/' . $sid) . '</div></div>'
             ;
-    
   }
-  echo '</div><br/>';
+  echo '</div></div>';
 }
-
 ?>
